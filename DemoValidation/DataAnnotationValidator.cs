d21d795cb7709aa74ee6ee50e915e3286120abde
Emmanuel DURIN <e_durin@hotmail.com>
2022-03-29 16:35:27 +0100

    Cleanup

--- DemoValidation/DataAnnotationValidator.cs
+++ DemoValidation/DataAnnotationValidator.cs
@@ -10,26 +10,6 @@ namespace DemoValidation
       Dictionary<string, List<string>> errors = new Dictionary<string, List<string>>();
       Type type = validationSource.GetType();
       PropertyInfo[] properties = type.GetProperties(BindingFlags.Public | BindingFlags.GetField | BindingFlags.Instance);
-      //foreach (var property in properties)
-      //{
-      //  var propertyName = property.Name;
-      //  var propertyValue = property.GetValue(validationSource);
-      //  List<ValidationResult> results = new List<ValidationResult>(1);
-      //  bool result = Validator.TryValidateProperty(
-      //      propertyValue,
-      //      new ValidationContext(validationSource, null, null)
-      //      {
-      //        MemberName = propertyName
-      //      },
-      //      results);
-      //  if (!result)
-      //  {
-      //    errors.Add(propertyName,
-      //      results.Select(vr => vr.ErrorMessage)
-      //             .OfType<string>()
-      //             .ToList());
-      //  }
-      //}
       return properties.ToDictionary(p => p.Name, p => GetFieldErrors(validationSource, p));
     }
     private static List<string> GetFieldErrors(object validationSource, PropertyInfo property)
